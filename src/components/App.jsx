642b82440f50c8973d9a28695c83612900da0c2f
Antony Córdoba <anfa2091@gmail.com>
2023-06-04 22:39:54 -0500

    Task1 finished

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,16 @@
+import jsonprofile from './task1/utils/user.json';
+import Profile from './task1/component/Profile';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <Profile
+        username={jsonprofile.username}
+        tag={jsonprofile.tag}
+        location={jsonprofile.location}
+        avatar={jsonprofile.avatar}
+        stats={jsonprofile.stats}
+      />
     </div>
   );
 };
---
+++ src/components/task1/component/Profile.jsx
@@ -0,0 +1,50 @@
+import './profile.css';
+import PropTypes from "prop-types";
+
+
+export const Profile = ({ username, tag, location, avatar, stats }) => {
+    
+  return (<div className="container">
+    <div className="profile">
+      <div className="description">
+        <img
+          src={avatar}
+          alt="User avatar"
+          className="avatar"
+          width="200px"
+        />
+        <p className="name"><b>{username}</b></p>
+        <p className="tag">{tag}</p>
+        <p className="location">{location}</p>
+        <ul className="stats">
+          <li className="label">
+            <span >Followers</span>
+            <span className="quantity"><b>{stats.followers}</b></span>
+          </li>
+          <li className="label">
+            <span>Views</span>
+            <span className="quantity"><b>{stats.views}</b></span>
+          </li>
+          <li className="label">
+            <span >Likes</span>
+            <span className="quantity"><b>{stats.likes}</b></span>
+          </li>
+        </ul>
+      </div>
+    </div>
+
+  </div>);
+}
+
+Profile.propTypes = {
+  username: PropTypes.string,
+  tag: PropTypes.string,
+  location: PropTypes.string,
+  avatar: PropTypes.string,
+  stats: PropTypes.object,
+};
+
+
+export default Profile
+
+
